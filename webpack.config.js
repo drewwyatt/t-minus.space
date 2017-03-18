5462cbd87c78ec354fc43d38a859a6516a2acde3
Drew Wyatt <drew.wyatt@bottlerocketstudios.com>
2017-03-18 12:14:09 -0500

    fixed webpack loader format, added cwd to webpack-elm-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const elmSource = __dirname + '/elm-stuff';
 
 module.exports = {
   entry: {
@@ -27,16 +28,28 @@ module.exports = {
       {
         test: /\.html$/,
         exclude: /node_modules/,
-        loader: "file-loader?name=[name].[ext]"
+        loader: "file-loader",
+        options: {
+            name: '[name].[ext]'
+        }
       },
       {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader: "elm-webpack-loader?verbose=true&warn=true"
+        loader: 'elm-webpack-loader',
+        options: {
+            verbose: true,
+            warn: true,
+            cwd: __dirname
+        },
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        loader: "url-loader",
+        options: {
+            limit: 10000,
+            mimetype: 'application/font-woff'
+        }
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
